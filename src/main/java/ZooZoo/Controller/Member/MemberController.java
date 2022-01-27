349cce68b07d10f93521c0a0b8c9f8900dca5e72
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2022-01-27 14:44:36 +0900

    게시판 기본 레이아웃 추출

--- src/main/java/ZooZoo/Controller/Member/MemberController.java
+++ src/main/java/ZooZoo/Controller/Member/MemberController.java
@@ -27,7 +27,7 @@ public class MemberController {
         if(session.getAttribute("loginDTO") != null) {
             return "LogMain";
         } else {
-            return "Main";
+            return "Board/meetings";
         }
     }
     // 로그인화면으로
